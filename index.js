be56a2f151a9641b5ef442a0a1228e24900a3cdb
zakhar bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-10-05 16:12:30 +0200

    mechanik generated

---
+++ index.js
@@ -0,0 +1,19 @@
+const exporess = require("express");
+const exHb = require("express-handlebars");
+const app = exporess();
+const port = process.env.PORT || 8080;
+
+app.engine("handlebars", exHb.engine({defaultLayout: "main"}))
+app.set("view engine", "handlebars");
+app.use(exporess.static("public/css"));
+
+app.get("/", (req, res) => {
+    res.render("home", {
+        title: "Documentik",
+        content: "This is a very special page"
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Serwer wystartował na porcie ${port}`)
+});
