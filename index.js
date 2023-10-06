e5f059574a5c5d50eafc0c120b982fd48b10b9fc
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-10-06 19:06:05 +0200

    full container

--- index.js
+++ index.js
@@ -7,12 +7,44 @@ app.engine("handlebars", exHb.engine({defaultLayout: "main"}))
 app.set("view engine", "handlebars");
 app.use(exporess.static("public/css"));
 
-app.get("/", (req, res) => {
+app.get("/home", (req, res) => {
     res.render("home", {
-        title: "Documentik",
-        content: "BODY"
+        title: "home | Makaron",
+        content: "BODY",
+        port: port,
+        nums: ["one", "two", 3, "4otyry"]
     })
-})
+});
+
+app.get("/menu", (req, res) => {
+    res.render("menu", {
+        title: "menu | Makaron"
+    })
+});
+
+app.get("/shop", (req, res) => {
+    res.render("shop", {
+        title: "shop | Makaron"
+    })
+});
+
+app.get("/recipes", (req, res) => {
+    res.render("recipes", {
+        title: "recipes | Makaron"
+    })
+});
+
+app.get("/chefs", (req, res) => {
+    res.render("chefs", {
+        title: "chefs | Makaron"
+    })
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        title: "contact | Makaron"
+    })
+}); 
 
 app.listen(port, () => {
     console.log(`Serwer wystartował na porcie ${port}`)
