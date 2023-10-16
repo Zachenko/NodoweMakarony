3bdaa33f2c23af34fe3c2a4b9c31dd4411c5ef6c
zakhar.bozhenko <zakhar.bozhenko@cyfroweszkoly.pl>
2023-10-16 22:24:11 +0200

    chefs addition

--- index.js
+++ index.js
@@ -55,7 +55,27 @@ app.get("/chefs", (req, res) => {
     res.render("chefs", {
         title: "chefs | Makaron",
         names: [
-            "Marco Polo"
+            {
+                name: "Thomas McNaughton",
+                fp: "Thomas McNaughton (born October 9, 1983) is an American chef, restaurateur, and cookbook writer.",
+                sp: "He is the chef and owner of Flour + Water, Penny Roma and Flour + Water Pasta Shop in San Francisco, California."
+            },
+            {
+                name: "Fabio Viviani",
+                fp: "Fabio Viviani (born October 10, 1978, in Florence, Italy) is a chef, restaurateur, and cook book writer who has appeared on reality television.",
+                sp: "In 2008 he appeared on the reality television competition series Top Chef, where he was voted the \"fan favorite\"; since then he has had a second career as a television host, on-air personality and product pitchman."
+            },
+            {
+                name: "Giorgio Locatelli",
+                fp: "Giorgio Locatelli is a famous Italian chef, author of the bestselling culinary book “Made in Italy”, owner of Locanda Locatelli - the most famous Italian restaurant outside Italy.",
+                sp: "Locanda Locatelli is not the only restaurant by Giorgio Locatelli. The other, Locatelli’s Pizza, is located near the American city of Houston.",
+                tp: "Another one - Ronda Locatelli - operates at the Atlantis The Palm resort, located on The Palm Jumeirah - a fantastic artificial island in the city of Dubai.."
+            },
+            {
+                name: "Massimo Bottura",
+                fp: "Massimo Bottura (Italian pronunciation: [ˈmassimo botˈtuːra]; born 30 September 1962) is an Italian restaurateur and the chef patron of Osteria Francescana, a three-Michelin-star restaurant based in Modena, Italy.",
+                sp: "It has been ranked No.1 on The World's 50 Best Restaurants list twice and is currently part of the Best of the Best list."
+            },
         ]
     })
 });
